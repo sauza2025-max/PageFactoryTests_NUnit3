afc38b47888b4a04ebf9cff1e4543eef9fbcfdbb
sauza2025-max <sauza2025@gmail.com>
2026-04-07 18:44:43 -0600

    Add project files.

---
+++ PageFactoryTests_NUnit3/BmiCalcPage.cs
@@ -0,0 +1,54 @@
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace PageFactoryTests_NUnit3
+{
+    public class BmiCalcPage
+    {
+        static string Url = "https://practice.expandtesting.com/bmi";
+        private static string Title = "BMI Calculator";
+
+        private IWebDriver driver;
+
+        public BmiCalcPage()
+        {
+            driver = new ChromeDriver();
+        }
+
+        public void Load()
+        {
+            driver.Navigate().GoToUrl(Url);
+        }
+
+        public void Close()
+        {
+            driver.Close();
+        }
+
+        public bool IsLoaded
+        {
+            get { return driver.Title.Equals(Title); }
+        }
+
+        public void CalculateBmi(String height, String weight)
+        {
+            driver.FindElement(By.Id("height")).Clear();
+            driver.FindElement(By.Id("height")).SendKeys(height);
+            driver.FindElement(By.Id("weight")).Clear();
+            driver.FindElement(By.Id("weight")).SendKeys(weight);
+            driver.FindElement(By.ClassName("btn btn-primary")).Click();
+        }
+
+        public String Bmi
+        {
+            get { return driver.FindElement(By.Id("BMI")).GetAttribute("value").ToString(); }
+        }
+
+        //public String BmiCategory
+        //{
+        //    get { return driver.FindElement(By.Name("bmi_category")).GetAttribute("value"); }
+        //}
+    }
+}
---
+++ PageFactoryTests_NUnit3/BmiCalcTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework; // Ensure NUnit is referenced
+
+namespace PageFactoryTests_NUnit3
+{
+    public class BmiCalcTests
+    {
+        [TestCase]
+        public void TestBmiCalculator()
+        {
+            BmiCalcPage bmiCalcPage = new BmiCalcPage();
+            bmiCalcPage.Load();
+            // Assert.IsTrue(bmiCalcPage.IsLoaded);
+            Thread.Sleep(2000);
+            bmiCalcPage.CalculateBmi("181", "80");
+            Thread.Sleep(2000);
+            Assert.IsTrue(bmiCalcPage.Bmi.Contains("24.4"));
+            //Assert.AreEqual("Normal", bmiCalcPage.BmiCategory);
+            bmiCalcPage.Close();
+        }
+    }
+}
